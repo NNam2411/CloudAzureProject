f8355570ee6b466b7a5ee5665305e15a230fb825
NNam2411 <nguyennam2002ro@gmail.com>
2022-11-28 21:56:17 +0700

    kkkkk

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
+@app.route('/hello')
 def hello():
-    return render_template('hello.html')
+    return render_template('test.html')
 
 @app.route('/test')
 def test():
-    return render_template('test.html')
+    return render_template('hello.html')
 # ENV = jinja2.Environment(loader=jinja2.FileSystemLoader(str(root_path / 'templates')))
 # template = ENV.get_template("index.html")
 if __name__ == '__main__':
