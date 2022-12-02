aa0fc1c3eef88895295c15386472e4f80381d957
NNam2411 <nguyennam2002ro@gmail.com>
2022-12-02 22:21:21 +0700

    0212.1

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+def base():
+    return render_template("home.html")
+@app.route("/home")
 def home():
-    return render_template('home.html')
-@app.route('/dm')
-def dm():
-    return render_template('dm.html')
-@app.route('/test')
+    return render_template("home.html")
+@app.route("/test")
 def test():
-    return render_template('test.html')
-@app.route('/hello')
+    return render_template("test.html")
+@app.route("/hello")
 def hello():
-    return render_template('hello.html')
-@app.route('/index')
+    return render_template("hello.html")
+@app.route("/index")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 if __name__ == '__main__':
     # app.run(host='127.0.0.1', port=5500, debug=True)
-    app.debug = True
-    app.run()
+    app.run(debug=True)
