82a3399fed5b3ab88cd477633792b054e145d0e6
NNam2411 <nguyennam2002ro@gmail.com>
2022-12-05 10:15:31 +0700

    0512.1

--- app.py
+++ app.py
@@ -5,24 +5,28 @@ app = Flask(__name__)
 @app.route("/")
 def base():
     return render_template("home.html")
+
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+
+@app.route("/base")
+def base():
+    return render_template("base.html")
+
 @app.route("/test")
 def test():
     return render_template("test.html")
+
 @app.route("/hello")
 def hello():
     return render_template("hello.html")
+
 @app.route("/index")
 def index():
     return render_template("index.html")
-@app.route("/nhan")
-def nhan():
-    return render_template("nhan.html")
-@app.route("/mobile")
-def mobile():
-    return render_template("mobile.html")
+
     
 if __name__ == '__main__':
     # app.run(host='127.0.0.1', port=5500, debug=True)
